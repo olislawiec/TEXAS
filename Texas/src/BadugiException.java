f2b445fb4374f0209eaab0b0b48fd5cffb16878f
DominikKuszczak <dominik.kuszczak@gmail.com>
2015-11-30 20:21:11 +0100

    Pierwsze zmiany

---
+++ Texas/src/BadugiException.java
@@ -0,0 +1,9 @@
+public class BadugiException extends Exception
+{
+        private static final long serialVersionUID = 1L;
+
+        BadugiException(String komunikat)
+        {
+                super(komunikat);
+        };
+};
---
+++ Texas/src/Table.java
@@ -0,0 +1,5 @@
+
+public class Table {
+	public Deck dk;
+	public Card cards[];
+}
