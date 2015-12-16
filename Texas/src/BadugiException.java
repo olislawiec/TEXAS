fbcbf98e194bef79557abae2644050355714aa9f
olislawiec <warslawiec@gmail.com>
2015-12-16 12:08:40 +0100

    commit

--- Texas/src/BadugiException.java
+++
@@ -1,9 +0,0 @@
-public class BadugiException extends Exception
-{
-        private static final long serialVersionUID = 1L;
-
-        BadugiException(String komunikat)
-        {
-                super(komunikat);
-        };
-};
